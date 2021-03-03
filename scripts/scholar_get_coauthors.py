39b91d2d363f9fac0eacbdb8d9828097b98df6b1
Pim Bongaerts <pim@calacademy.org>
2021-03-04 11:26:04 +1300

    Slight refactoring

--- scripts/scholar_get_coauthors.py
+++ scripts/scholar_get_coauthors.py
@@ -25,36 +25,35 @@ def reformat_name(author_name):
 def get_all_coauthors(author_name, min_year, max_year, max_coauthors, 
                       include_no_year):
     """ Get a set of all coauthors """
-    search_query = scholarly.search_author(author_name)
-    author = scholarly.fill(next(search_query))
+    author = scholarly.fill(next(scholarly.search_author(author_name)))
     all_coauthors = set()
     for pub in author['publications']:
-        # Evaluate if publication year is indicated (if not,
-        # ignore depending on presence of --include_no_year flag)
+        # Evaluate if publication year is indicated (if not, ignore depending
+        # on presence of --include_no_year flag)
         if 'pub_year' in pub['bib']:
             pub_year = int(pub['bib']['pub_year'])
         elif include_no_year:
             pub_year = max_year
         else:
             pub_year = min_year - 1
+
         # Evaluate whether publication falls within indicated timerange
         if min_year <= pub_year <= max_year:
-            pub_details = scholarly.fill(pub)
-            coauthors = pub_details['bib']['author'].split(' and ')
-            # Evaluate if number of coauthors is above optional threshold
+            coauthors = scholarly.fill(pub)['bib']['author'].split(' and ')
+            # Evaluate if number of coauthors meets optional threshold
             if len(coauthors) <= max_coauthors:
-                for coauthor in coauthors:
-                    all_coauthors.add(reformat_name(coauthor))
+                [all_coauthors.add(reformat_name(coauthor)) for coauthor in coauthors]             
     return all_coauthors
 
 def output_coauthor_list(all_coauthors):
     """ Output formatted list of co-authors - and prepend 'A:' for NSF COA """
-    for coauthor in sorted(all_coauthors):
-        print('A:\t{0}'.format(coauthor))
+    [print('A:\t{0}'.format(coauthor)) for coauthor in sorted(all_coauthors)]
 
 def main(author_name, min_year, max_year, max_coauthors, include_no_year):
+    # Get the set of coauthors from Google Scholar
     all_coauthors = get_all_coauthors(author_name, min_year, max_year, 
                                       max_coauthors, include_no_year)
+    # Output formatted list of co-authors
     output_coauthor_list(all_coauthors)
 
 if __name__ == '__main__':
@@ -71,7 +70,8 @@ if __name__ == '__main__':
                               to be considered (optional; e.g. 30)')
     parser.add_argument('-i', '--include_no_year',
                         action = 'store_true',
-                        help='include publications without a year')
+                        help='include publications without a year (not \
+                        included by default)')
     args = parser.parse_args()
     main(args.author_name, int(args.min_year), int(args.max_year), 
          int(args.max_coauthors), args.include_no_year)
