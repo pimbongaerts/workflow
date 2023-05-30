9e3dcb6b0a2755c9efc9fef6d2f44c59614c5f7e
Pim Bongaerts <pim@calacademy.org>
2023-05-30 12:58:06 +1200

    Added go_to_model_folder script

--- scripts/go_to_model_folder.py
+++ scripts/go_to_model_folder.py
@@ -10,7 +10,7 @@ BASEPATH = "/mnt/coral3d/focal_plots"
 model_arg = sys.argv[1]
 cols = model_arg.split('_')
 if len(cols) == 4:
-    target_path = '{0}/{1}/{1}_{2}/{1}_{2}_{3}/{1}_{2}_{3}_{4}/'.format(BASEPATH,
+    target_path = '{0}/{1}_{2}/{1}_{2}_{3}/{1}_{2}_{3}_{4}/'.format(BASEPATH,
                                                                        cols[0], cols[1], cols[2], cols[3])
 elif len(cols) == 3:
     target_path = '{0}/{1}_{2}/{1}_{2}_{3}/'.format(BASEPATH, cols[0], cols[1], cols[2])
